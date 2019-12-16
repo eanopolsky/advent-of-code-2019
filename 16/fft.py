8f9e82d1fc63d914aed8d3c33becfc49d0d09a05
Eric Anopolsky <erpo41@gmail.com>
2019-12-15 22:16:14 -0700

    generates repeating patterns correctly

--- 16/fft.py
+++ 16/fft.py
@@ -3,9 +3,17 @@
 with open("signal.txt","r") as f:
     signal = [int(char) for char in f.readline().rstrip()]
 
-#print(signal)
+#print(signal) #works
 
 basepattern = [0,1,0,-1]
 
-def getpatternforelement(bp,element):
-    pass
+def getpatternforelement(bp,elementnum,signallength):
+    singlepattern = []
+    for val in bp:
+        singlepattern.extend([val for i in range(elementnum)])
+    newpattern = singlepattern[1:]
+    while len(newpattern) < signallength:
+        newpattern.extend(singlepattern)
+    return newpattern[0:signallength]
+
+print(getpatternforelement(basepattern,3,8))
