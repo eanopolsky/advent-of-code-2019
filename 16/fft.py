c6ef2b58b59fa4cacd21a7b2de8901426d5cc679
Eric Anopolsky <erpo41@gmail.com>
2019-12-15 22:33:11 -0700

    solves day 16 part 1

--- 16/fft.py
+++ 16/fft.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-with open("signal.txt","r") as f:
+with open("myinput.txt","r") as f:
     signal = [int(char) for char in f.readline().rstrip()]
 
 #print(signal) #works
@@ -16,4 +16,23 @@ def getpatternforelement(bp,elementnum,signallength):
         newpattern.extend(singlepattern)
     return newpattern[0:signallength]
 
-print(getpatternforelement(basepattern,3,8))
+#print(getpatternforelement(basepattern,3,8)) #works
+for phase in range(100):
+    #print("phase {}".format(phase+1))
+    #print("input signal: {}".format(signal))
+    nextsignal = []
+    for i in range(len(signal)):
+        #print("i {}".format(i))
+        pattern = getpatternforelement(basepattern,i+1,len(signal))
+        #print("pattern: {}".format(pattern))
+        sum = 0
+        for j in range(len(signal)):
+            #print("j {}".format(j))
+            sum += signal[j]*pattern[j]
+        sum = abs(sum) % 10
+        nextsignal.append(sum)
+    signal = nextsignal
+    #print("new signal: {}".format(signal))
+    #input()
+
+print(signal[0:8])
