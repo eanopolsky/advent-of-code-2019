4e89aeadb1ab02760fc59d39a3632d89140fb3b5
Eric Anopolsky <erpo41@gmail.com>
2019-12-16 18:42:39 -0700

    solves part 1 in roughly half the time as fft.py

--- 16/fft3.py
+++ 16/fft3.py
@@ -5,10 +5,13 @@ with open("myinput.txt","r") as f:
 #print(signal) #works
 
 #part 2:
-realsignal = []
-for i in range(10000):
-    realsignal.extend(signal)
-signal = realsignal
+part2 = False
+if part2:
+    messageoffset = signal[0:7]
+    realsignal = []
+    for i in range(10000):
+        realsignal.extend(signal)
+    signal = realsignal
 
 
 #rangelensignal = range(len(signal)) # this actually makes it run slower
@@ -20,15 +23,26 @@ for phase in range(100):
         # i   is the index in the signal array in this program
         # i+1 is the "position in the output list" in the language of
         #     the problem
-        print("i {}".format(i))
-        positiveelements = [signal[j] for j in range(len(signal)) if (((j+1)//(i+1)) % 4) == 1]
-        negativeelements = [signal[j] for j in range(len(signal)) if (((j+1)//(i+1)) % 4) == 3]
-        #print("positiveelements: {}".format(positiveelements))
-        #print("negativeelements: {}".format(negativeelements))
-        esum = sum(positiveelements) - sum(negativeelements)
-        esum = abs(esum) % 10
-        #print("esum: {}".format(esum))
-        nextsignal.append(esum)
+        #
+        # build an nextsignal backwards
+        #if i % 10000 == 0:
+        #print("i: {}".format(i))
+        if i == 0:
+            #last element of the signal never changes
+            nextsignal.append(signal[len(signal)-1]) 
+        elif i < (len(signal)/2):
+            #no need to account for negative numbers before % 10
+            #because all summands are positive
+            nextvalue = (nextsignal[i-1] + signal[len(signal)-1-i]) % 10
+            nextsignal.append(nextvalue)
+        else:
+            i = len(signal)-1-i #try the old logic for now
+            positiveelements = [signal[j] for j in range(len(signal)) if (((j+1)//(i+1)) % 4) == 1]
+            negativeelements = [signal[j] for j in range(len(signal)) if (((j+1)//(i+1)) % 4) == 3]
+            esum = sum(positiveelements) - sum(negativeelements)
+            esum = abs(esum) % 10
+            nextsignal.append(esum)
+    nextsignal.reverse() #account for list built backwards
     signal = nextsignal
     #print("new signal: {}".format(signal))
     #input()
