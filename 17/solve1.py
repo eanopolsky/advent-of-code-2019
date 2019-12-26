3ee13a56c5a3e7fd067ef990b4d8f1289880aead
Eric Anopolsky <erpo41@gmail.com>
2019-12-25 18:50:34 -0700

    solves day 17 part 1

--- 17/solve1.py
+++ 17/solve1.py
@@ -9,3 +9,25 @@ myfb = intcodevm.asciifb()
 myvm.setoutputfunc(myfb.receiveint)
 myvm.run()
 myfb.render()
+
+alignparams = []
+for pixelcoords in myfb.screenmap:
+    if myfb.screenmap[pixelcoords] != "#":
+        #not an intersection
+        continue
+    neighbors = [(pixelcoords[0]-1,pixelcoords[1]),
+                 (pixelcoords[0]+1,pixelcoords[1]),
+                 (pixelcoords[0],pixelcoords[1]-1),
+                 (pixelcoords[0],pixelcoords[1]+1)]
+    try:
+        nscaff = [n for n in neighbors if myfb.screenmap[n] == "#"]
+        if len(nscaff) == 4:
+            alignparam = pixelcoords[0]*pixelcoords[1]
+            alignparams.append(alignparam)
+        else:
+            continue
+    except KeyError:
+        #this pixel is on the edge of the screen
+        continue
+
+print(sum(alignparams))
