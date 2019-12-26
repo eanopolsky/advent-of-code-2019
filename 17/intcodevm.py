5d8ad2ebe419627142460646296190a2bc024d72
Eric Anopolsky <erpo41@gmail.com>
2019-12-25 18:37:11 -0700

    add ascii framebuffer

--- 17/intcodevm.py
+++ 17/intcodevm.py
@@ -37,6 +37,37 @@ class memorymanager:
     def __setitem__(self,key,value):
         self.memdict[key] = value
 
+class asciifb:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.screenmap = {} #element = (0,0): "#"
+    def receiveint(self, n):
+        if type(n) != type(int()):
+            print("wrong type passed to receiveint")
+            exit(1)
+        if n == 10:
+            self.y += 1
+            self.x = 0
+            return
+        self.screenmap[(self.x,self.y)] = chr(n)
+        self.x += 1
+    def render(self):
+        rx = 0
+        ry = 0
+        while True: #iterate over lines
+            while True: #iterate over line
+                try:
+                    print(self.screenmap[(rx,ry)],end="")
+                except KeyError:
+                    print("")
+                    break
+                rx += 1
+            ry += 1
+            rx = 0
+            if (rx,ry) not in self.screenmap:
+                break
+
 class intcodevm:
     #instructions
     decodemap = {1: {"name":"add","parameters":3},
--- 17/solve1.py
+++ 17/solve1.py
@@ -5,5 +5,7 @@ import intcodevm
 with open('myinput.txt') as f:
     memory = [int(x) for x in f.readline().split(",")]
 myvm = intcodevm.intcodevm(memory = memory, name = "ascii")
-myvm.setoutputmode("printascii")
+myfb = intcodevm.asciifb()
+myvm.setoutputfunc(myfb.receiveint)
 myvm.run()
+myfb.render()
