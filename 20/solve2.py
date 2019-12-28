2416f72669f94dc53ef5430639fe96595d358533
Eric Anopolsky <erpo41@gmail.com>
2019-12-27 23:14:07 -0700

    finds dimensions of inner ring

--- 20/solve2.py
+++ 20/solve2.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
+from copy import deepcopy
+
 #debug = False
 #debug = True
 
-#inputfile = "sample1.txt"
+inputfile = "sample1.txt"
 #inputfile = "sample2.txt"
-inputfile = "myinput.txt"
+#inputfile = "myinput.txt"
 
 from asciifb import asciifb
 
@@ -18,11 +20,55 @@ with open(inputfile,"r") as f:
 
 mymap = myfb.getmap()
 
+#find dimensions of map
+#and boundaries of inner ring/rectangle
+width = 0
+height = 0
+for loc in mymap:
+    if loc[0] > width:
+        width = loc[0]
+    if loc[1] > height:
+        height = loc[1]
+center = (int(width/2),int(height/2))
+#print(center)
+innerring = {}
+x = center[0]
+while True:
+    if mymap[(x,center[1])]["ch"] == "." or mymap[(x,center[1])]["ch"] == "#":
+        innerring["startx"] = x
+        break
+    else:
+        x -= 1
+x = center[0]
+while True:
+    if mymap[(x,center[1])]["ch"] == "." or mymap[(x,center[1])]["ch"] == "#":
+        innerring["endx"] = x
+        break
+    else:
+        x += 1
+y = center[1]
+while True:
+    if mymap[(center[0],y)]["ch"] == "." or mymap[(center[0],y)]["ch"] == "#":
+        innerring["starty"] = y
+        break
+    else:
+        y -= 1
+y = center[1]
+while True:
+    if mymap[(center[0],y)]["ch"] == "." or mymap[(center[0],y)]["ch"] == "#":
+        innerring["endy"] = y
+        break
+    else:
+        y += 1
+print(innerring)
+exit(1)
+
 # tag portal-adjacent spaces
 letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 for loc in mymap:
     if mymap[loc]["ch"] == ".":
         portal = ""
+        portaldir = ""
         if mymap[(loc[0]-1,loc[1])]["ch"] in letters:
             portal = mymap[(loc[0]-2,loc[1])]["ch"] + mymap[(loc[0]-1,loc[1])]["ch"]
         elif mymap[(loc[0]+1,loc[1])]["ch"] in letters:
@@ -33,32 +79,39 @@ for loc in mymap:
             portal = mymap[(loc[0],loc[1]+1)]["ch"] + mymap[(loc[0],loc[1]+2)]["ch"]
         else:
             continue
-        if portal == "AA":
-            mymap[loc]["start"] = True
-        elif portal == "ZZ":
-            mymap[loc]["end"] = True
-        else:
-            mymap[loc]["portal"] = portal
+        mymap[loc]["portal"] = portal
 
-# #portal tag check:
-#
-# for loc in mymap:
-#     try:
-#         print(mymap[loc]["portal"])
-#     except KeyError:
-#         pass
-# exit(1)
+originalmap = mymap
+layers = []
 
+portalnames = set()
+for loc in mymap:
+    if "portal" in mymap[loc]:
+        portalnames.add(mymap[loc]["portal"])
+
+def addlayer():
+    newlayer = deepcopy(originalmap)
+    layernum = len(layers)
+    layers.append(newlayer)
+    if layernum == 0:
+        for loc in newlayer:
+            if newlayer[loc]["portal"] == "AA":
+                newlayer[loc]["start"] = True
+            if newlayer[loc]["portal"] == "ZZ":
+                newlayer[loc]["end"] = True
+    else:
+        prevlayer = layernum - 1
+        #incomplete. needs portal linking between this
+        #layer and previous layer
+                
+
+exit(1)
 for loc in mymap:
     if "start" in mymap[loc]:
         startloc = loc
     if "end" in mymap[loc]:
         endloc = loc
 
-portalnames = set()
-for loc in mymap:
-    if "portal" in mymap[loc]:
-        portalnames.add(mymap[loc]["portal"])
 
 for portalname in portalnames:
     connectedspaces = [loc for loc in mymap if "portal" in mymap[loc] and mymap[loc]["portal"] == portalname]
