83bf90cbe3c02a7d2d753750b69836df93b08fe8
Eric Anopolsky <erpo41@gmail.com>
2019-12-27 19:52:50 -0700

    copies of previous solutions

---
+++ 19/solve2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import intcodevm
+import asciifb
+
+with open("myinput.txt") as f:
+    memory = [int(x) for x in f.readline().split(",")]
+
+beamfb = asciifb.asciifb()
+
+affectedpoints = 0
+for y in range(50):
+    for x in range(50):
+        beamscanner = intcodevm.intcodevm(memory=memory,name="beamscanner")
+        beamscanner.setinputmode("queue")
+        beamscanner.queueinput(x)
+        beamscanner.queueinput(y)
+        beamscanner.setoutputmode("queue")
+        beamscanner.run()
+        affectedpoints += beamscanner.getoutput()
+print(affectedpoints)
