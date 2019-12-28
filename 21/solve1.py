a8edb0f1d25def46662ced1eeadacd67a40a4973
Eric Anopolsky <erpo41@gmail.com>
2019-12-28 15:43:09 -0700

    solves day 21 part 1

--- 21/solve1.py
+++ 21/solve1.py
@@ -6,6 +6,17 @@ with open("myinput.txt") as f:
     memory = [int(x) for x in f.readline().split(",")]
 
 springdroid = intcodevm.intcodevm(memory=memory,name="springdroid")
-springdroid.setinputmode("ascii")
+springdroid.setinputmode("queue")
 springdroid.setoutputmode("printascii")
+springscript = """NOT A T
+OR T J
+NOT B T
+OR T J
+NOT C T
+OR T J
+AND D J
+WALK
+"""
+for char in springscript:
+    springdroid.queueinput(ord(char))
 springdroid.run()
