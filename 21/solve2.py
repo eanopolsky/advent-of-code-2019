613281c89784ed797c714343cdd6ac35ed68e529
Eric Anopolsky <erpo41@gmail.com>
2019-12-28 16:21:22 -0700

    solves day 21 part 2

--- 21/solve2.py
+++ 21/solve2.py
@@ -14,7 +14,11 @@ springscript = [ "NOT A T",
                  "OR T J",
                  "NOT C T",
                  "OR T J",
-                 "AND D J",
+                 "AND D J", #set J if there are gaps coming and D is hull
+                 "NOT E T",
+                 "NOT T T", #set T to E (space after landing)
+                 "OR H T",  #T = droid can advance to E or jump to H
+                 "AND T J",
                  "RUN"]
 for line in springscript:
     for char in line:
